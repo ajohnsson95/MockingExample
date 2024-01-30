a60295e37bb9f7a65aece38bf3a7193f68c3f176
Adam Johnsson <ajohnsson95@hotmail.com>
2024-01-30 14:11:57 +0100

    added test for adding two numbers with result and fixed conditions in StringCalculator for test to pass

--- src/main/java/string/calculator/StringCalculator.java
+++ src/main/java/string/calculator/StringCalculator.java
@@ -5,6 +5,10 @@ public class StringCalculator {
         if (numbers.isEmpty()){
             return 0;
         }
-        return Integer.parseInt(numbers);
+        String[] parts = numbers.split(",");
+        if (parts.length == 1) {
+            return Integer.parseInt(parts[0]);
+        }
+        return Integer.parseInt(parts[0]) + Integer.parseInt(parts[1]);
     }
 }
--- src/test/java/string/calculator/StringCalculatorTests.java
+++ src/test/java/string/calculator/StringCalculatorTests.java
@@ -11,8 +11,16 @@ public class StringCalculatorTests {
     void testAddEmptyString() {
         assertEquals(0, StringCalculator.add(""));
     }
+
     @Test
-    public void testAddSingleNumber() {
+    @DisplayName("test add single number")
+    void testAddSingleNumber() {
         assertEquals(5, StringCalculator.add("5"));
+    }
+
+    @Test
+    @DisplayName("test add two numbers")
+    void testAddTwoNumbers() {
+        assertEquals(8, StringCalculator.add("3,5"));
     }
 }
